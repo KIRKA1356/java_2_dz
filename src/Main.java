98229c384bdbf8d6fe1b327296d498a024305bdc
KIRKA1356 <kgritsun@mail.ru>
2023-01-29 16:17:21 +0300

    over ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.logging.Logger;
+
+/*
+Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void bubbleSort(int[] sortArr) {
+        for (int i = 0; i < sortArr.length - 1; i++) {
+            for (int j = 0; j < sortArr.length - i - 1; j++) {
+                if (sortArr[j + 1] < sortArr[j]) {
+                    int swap = sortArr[j];
+                    sortArr[j] = sortArr[j + 1];
+                    sortArr[j + 1] = swap;
+                    Logger log = Logger.getAnonymousLogger();
+                    log.info("We swap elements");
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        bubbleSort(sortArr);
+        for (int i = 0; i < sortArr.length; i++) {
+            System.out.print(sortArr[i] + "\n");
+        }
     }
 }
